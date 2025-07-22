8075d7298906ccc9b57aa115167101da3bc9eb6b
Nicholas Noochla-or <nnlaor@gmail.com>
2025-07-21 20:58:00 -0400

    adding new architecture

--- trade_lever/calculator/__init__.py
+++ trade_lever/calculator/__init__.py
@@ -1,5 +1,6 @@
+from .position import calculate_position, calculate_shares
+
 from .margin import calculate_margin, calculate_leverage
-from .position_size import calculate_position_size, calculate_shares
 from .profit_loss import calculate_pnl, calculate_breakeven
 from .risk_management import calculate_risk_per_trade, calculate_max_position_size
 from .utilities import *  # or import specific utility functions
@@ -7,7 +8,7 @@ from .utilities import *  # or import specific utility functions
 __all__ = [
     'calculate_margin',
     'calculate_leverage', 
-    'calculate_position_size',
+    'calculate_position',
     'calculate_shares',
     'calculate_pnl',
     'calculate_breakeven',
